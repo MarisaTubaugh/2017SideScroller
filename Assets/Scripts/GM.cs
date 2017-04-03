235834f4760a2e1b3e29e0648350c41a2d98d0bf
Marisa Tubaugh <tubaughm1@hawkmail.newpaltz.edu>
2017-04-03 11:36:14 -0400

    Made LivesValue active when player gets out

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour {
+	private int _Lives = 3;
+	public int points;
+
+	public Text livesValue;
+
+
+	// Use this for initialization
+	public void SetLives(int newValue) {
+		_Lives = newValue;
+		Debug.Log("Lives now equals: " + _Lives);
+		livesValue.text = _Lives.ToString();
+	}
+	public int GetLives()
+	{
+		return _Lives;
+	}
+}
