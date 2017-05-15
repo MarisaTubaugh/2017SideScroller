57ce78b613b0107cdec27f780477dc5be0085a92
Marisa Tubaugh <tubaughm1@hawkmail.newpaltz.edu>
2017-05-15 12:18:15 -0400

    FINAL EXAM
    Attempted to make a working warp pipe, added "Teleport" script.

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+	public Transform newPos;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+			other.position = newPos.position; (-6.1788, 1.035913, 0);
+	}
+}
